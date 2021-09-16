9c31ca05928ca23e9e325f8f70b1350e2d411ec6
Wijdan Muhammad R <42838967+youudan@users.noreply.github.com>
2021-09-16 10:28:18 +0700

    menyelesaikan video ke 2, membuat property dan method

--- produk.php
+++ produk.php
@@ -1,12 +1,39 @@
 <?php
-
-// Jualan Produk
-// Komik
-// Game
-
 class Produk
 {
+  public $judul = "judul",
+    $penulis = "penulis",
+    $penerbit = "pernebit",
+    $harga = 0;
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckermann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 25000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "</br>";
+echo "Game : " . $produk4->getLabel();
