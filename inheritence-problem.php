ff39d17d653ac2a81b795b38dc45e2b1cb805108
Wijdan Muhammad R <42838967+youudan@users.noreply.github.com>
2021-09-23 09:22:27 +0700

    pertemuan 3, video 1 - inheritance (part 1)

---
+++ inheritence-problem.php
@@ -0,0 +1,56 @@
+<?php
+class Produk
+{
+  public $judul,
+    $penulis,
+    $penerbit,
+    $harga,
+    $jmlHalaman,
+    $waktuMain,
+    $tipe;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe = "tipe")
+  {
+    $this->judul      = $judul;
+    $this->penulis    = $penulis;
+    $this->penerbit   = $penerbit;
+    $this->harga      = $harga;
+    $this->jmlHalaman = $jmlHalaman;
+    $this->waktuMain  = $waktuMain;
+    $this->tipe       = $tipe;
+  }
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+
+  public function getInfoLengkap()
+  {
+    $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} ({$this->harga})";
+    if ($this->tipe === "Komik") {
+      $str .= " - {$this->jmlHalaman} Halaman.";
+    } else if ($this->tipe === "Game") {
+      $str .= " ~ {$this->waktuMain} Jam.";
+    }
+
+    return $str;
+  }
+}
+
+class CetakInfoProduk
+{
+  public function cetak(Produk $produk)
+  {
+    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+    return $str;
+  }
+}
+
+$produk1 = new Produk("Bokutachi no Remake", "Kio Nachi", "Media Factory", 65000, 100, 0, "Komik");
+$produk2 = new Produk("Summer Pockets", "Jun Maeda", "VisualArts", 189999, 0, 50, "Game");
+
+echo $produk1->getInfoLengkap();
+echo "</br>";
+echo $produk2->getInfoLengkap();
+echo "</br>";
